0f78d485b9268da7d317d74b3649c587ff4d3061
이상남 <lsn5963@naver.com>
2022-11-16 23:43:47 +0900

    feat: entity 추가

---
+++ src/main/java/TeamProject/demo/domain/account/entity/Entity.java
@@ -0,0 +1,10 @@
+package TeamProject.demo.domain.account.entity;
+
+import lombok.Getter;
+
+@javax.persistence.Entity
+@Getter
+public class Entity {
+
+    @
+}
---
+++ src/main/java/TeamProject/demo/domain/history/entity/Entity.java
@@ -0,0 +1,10 @@
+package TeamProject.demo.domain.history.entity;
+
+import lombok.Getter;
+
+@javax.persistence.Entity
+@Getter
+public class Entity {
+
+    @
+}
