a6b4e97b0b36121a97c97421c578221cd609aea4
lsn5963 <lsn5963@naver.com>
2022-11-17 16:13:23 +0900

    feat: Test 추가

--- src/test/java/TeamProject/demo/DemoApplicationTests.java
+++ src/test/java/TeamProject/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package TeamProject.demo;
 
+import TeamProject.demo.domain.client.entity.Client;
+import TeamProject.demo.domain.client.repository.ClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	ClientRepository clientRepository;
 	@Test
 	void contextLoads() {
+		Client client = new Client();
+		client.setAddress("용인");
+//		client.setBirth("");
+		client.setEmail("lsn@sadlkj");
+		client.setJob("학생");
+		client.setName("이");
+		client.setRegNumber("01213-56547");
+
+		clientRepository.save(client);
 	}
 
 }
